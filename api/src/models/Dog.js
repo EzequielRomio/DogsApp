a037bc4f8ccc79dd60ff811536374e5138bbc693
EzequielRomio <romio.ezequiel@gmail.com>
2021-04-12 18:45:02 -0300

    Starting on backend, dog table updated

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -1,12 +1,36 @@
 const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
+// "height":{"imperial":"9 - 11.5","metric":"23 - 29"}
+// Peso *         // "weight":{"imperial":"6 - 13","metric":"3 - 6"}
+// Años de vida   // "life_span":"10 - 12 years"
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('dog', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    height: {
+      type: DataTypes.JSON, // example "height":{"imperial":"9 - 11.5","metric":"23 - 29"}
+      allowNull: false,
+    },
+    weight : {
+      type: DataTypes.JSON, // example "weight":{"imperial":"6 - 13","metric":"3 - 6"}
+      allowNull: false,
+    },
+    life_span: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    }, 
+    external_api_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    }
   });
 };
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -7,6 +7,12 @@ const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+router.get('/', function(req, res){
+    return res.status(200).json({"dog": "water"})
+})
 
+router.get('/dogs', function(req, res){
+    return res.status(200).json({"dog": "water"})
+})
 
 module.exports = router;
