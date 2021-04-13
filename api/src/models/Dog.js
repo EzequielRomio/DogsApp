165a795c156d76532aeaa0e8149b23ea060f47d2
EzequielRomio <romio.ezequiel@gmail.com>
2021-04-13 14:33:57 -0300

    Models defined

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -1,35 +1,28 @@
-const { DataTypes } = require('sequelize');
+// const { DataTypes } = require('sequelize');
+const S = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
-// "height":{"imperial":"9 - 11.5","metric":"23 - 29"}
-// Peso *         // "weight":{"imperial":"6 - 13","metric":"3 - 6"}
-// Años de vida   // "life_span":"10 - 12 years"
+
 module.exports = (sequelize) => {
-  // defino el modelo
   sequelize.define('dog', {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true
-    },
     name: {
-      type: DataTypes.STRING,
+      type: S.STRING,
       allowNull: false,
     },
     height: {
-      type: DataTypes.JSON, // example "height":{"imperial":"9 - 11.5","metric":"23 - 29"}
+      type: S.JSON, // example "height":{"imperial":"9 - 11.5","metric":"23 - 29"}
       allowNull: false,
     },
     weight : {
-      type: DataTypes.JSON, // example "weight":{"imperial":"6 - 13","metric":"3 - 6"}
+      type: S.JSON, // example "weight":{"imperial":"6 - 13","metric":"3 - 6"}
       allowNull: false,
     },
     life_span: {
-      type: DataTypes.STRING,
+      type: S.STRING,
       allowNull: true,
     }, 
     external_api_id: {
-      type: DataTypes.INTEGER,
+      type: S.INTEGER,
       allowNull: true,
     }
   });
--- api/src/models/Temperament.js
+++ api/src/models/Temperament.js
@@ -2,11 +2,6 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('temperament', {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-      primaryKey: true
-    },
     name: {
       type: DataTypes.STRING,
       allowNull: true,
