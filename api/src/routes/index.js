1afe6d04ec322bce7a97fb7a3ecca34f2f885e21
EzequielRomio <romio.ezequiel@gmail.com>
2021-04-14 16:19:24 -0300

    uuid implemented

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -16,25 +16,5 @@ router.use('/temperament', temperamentRoutes);
 router.use('/dogs', dogRoutes);
 router.use('/dog', dogRoutes);
 
-const fillDataBase = () => {
-    // import axios from axios, Dog from db.js
-    axios.get('https://api.thedogapi.com/v1/breeds', {responseType: 'json'})
-        .then(r =>{
-            r.data.forEach(newDog => {
-                Dog.create({
-                    name: newDog.name,
-                    height: newDog.height,
-                    weight: newDog.weight,
-                    life_span: newDog.life_span,
-                    external_api_id: newDog.id        
-                });
-            });
-        })
-        .catch(err => {
-            console.log(err);
-        });   
-
-}
-//fillDataBase()
 
 module.exports = router;
