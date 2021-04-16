f436b63411a41eef69c48e93c57054a9eeaf8fc4
viru <ezequielprovision@hotmail.com>
2021-04-16 08:11:06 -0300

    get methods returns relationated tables

--- api/src/controllers/dog.js
+++ api/src/controllers/dog.js
@@ -8,7 +8,7 @@ function getDogs(req, res, next) {
     console.log(name)
     if (!name) {
         const dogsApi = axios.get('https://api.thedogapi.com/v1/breeds', {responseType: 'json'});
-        const dogsDB = Dog.findAll();
+        const dogsDB = Dog.findAll({include: [Temperament]});
         Promise.all([dogsApi, dogsDB])
             .then(dogs => {
                 let [dogsApiResult, dogsDBResult] = dogs;
@@ -18,7 +18,7 @@ function getDogs(req, res, next) {
             .catch(err => next(err));
 
     } else {
-        const dogsDB = Dog.findAll({where: {name: name}})
+        const dogsDB = Dog.findAll({where: {name: name}, include: [Temperament]})
         const dogsApi = axios.get(`https://api.thedogapi.com/v1/breeds/search?q=${name}`)
         Promise.all([dogsApi, dogsDB])
             .then(dogs => {
@@ -35,8 +35,9 @@ function getDogs(req, res, next) {
 
 function getDog(req, res, next) {
     let idNum = req.params.id;
-    if (idNum.includes('-') && !parseInt(idNum)) {
-        Dog.findOne({where: {id: idNum}}).then(dog => {
+    if (idNum.includes('a') || idNum.includes('b') || idNum.includes('c') || idNum.includes('d') || idNum.includes('e')) {
+        Dog.findOne({where: {id: idNum}, include: [Temperament]}).then(dog => {
+            console.log(dog)
             if (dog) {
                 return res.json(dog)
             } else {
@@ -73,9 +74,13 @@ async function addDog(req, res, next) {
             const temperaments = newDog.temperament.replace(/,/g, '').split(' ');
             for (const temperament of temperaments) {
                 try {
-                    const postedTemperament = await Temperament.create({name: temperament});  
-                    await postedDog.addTemperament(postedTemperament);
-                } catch {}
+                    //const postedTemperament = await Temperament.create({name: temperament});
+                    const postedTemperament = await Temperament.findOrCreate({where: {name: temperament}})
+                    //console.log(postedTemperament)  
+                    await postedDog.addTemperament(postedTemperament[0].dataValues.id);
+                } catch (err) {
+                    console.log(err)
+                }
             }
             return res.status(200).json(postedDog)
         } else {
--- api/src/controllers/temperament.js
+++ api/src/controllers/temperament.js
@@ -4,7 +4,7 @@ const {v4: uuidv4} = require('uuid');
 const {Temperament, Dog} = require('../db.js');
 
 function getTemperaments(req, res, next) {
-    return Temperament.findAll()
+    return Temperament.findAll({include: [Dog]})
         .then(temperaments => res.json({temperaments}))
         .catch(err => next(err));
 }
@@ -16,23 +16,6 @@ function addTemperament(req, res, next) {
         .catch(err => next(err));
 }
 
-// function fillDB() {
-//     axios.get('https://api.thedogapi.com/v1/breeds', {responseType: 'json'})
-//     .then(res => {
-//         const dogs = res.data;
-//         for (const d of dogs) {
-//             //"Aloof, Clownish, Dignified, Independent, Happy"
-//             if (d.temperament) {
-//                 const temperaments = d.temperament.split(' ');
-//                 for (let tempe of temperaments) {
-//                     tempe = tempe.replace(',', '');
-//                     Temperament.findOrCreate({where: {name: tempe}});
-//                 }
-//             }
-//         }
-//     })
-//     .catch(err => console.error(err))
-// }
 
 async function fillDB () {
     try {
@@ -41,14 +24,17 @@ async function fillDB () {
             //"Aloof, Clownish, Dignified, Independent, Happy"
             if (dog.temperament && dog.name) {
                 const temperaments = dog.temperament.replace(/,/g, '').split(' ');
+                const id = uuidv4();
+                const newDog = {...dog, id};
+                const postedDog = await Dog.create(newDog); 
                 for (const temperament of temperaments) {
                     try {
-                        const id = uuidv4();
-                        const newDog = {...dog, id} 
-                        const postedTemp = await Temperament.create({name: temperament});
-                        const postedDog = await Dog.create(newDog)
-                        await postedTemp.addDog(postedDog)
-                    } catch {}
+                        const postedTemp = await Temperament.findOrCreate({where: {name: temperament}});
+                        await postedTemp[0].addDog(postedDog)
+                        await postedDog.addTemperament(postedTemp[0].dataValues.id)
+                    } catch (err) {
+                        console.log(err)
+                    }
                 }
             }
         }
